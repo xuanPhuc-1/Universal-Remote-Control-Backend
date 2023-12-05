316199bbb702a19636d87a60c2fb98982fa8d86f
xuanPhuc-1 <boneman1782002@gmail.com>
2023-12-05 19:38:31 +0700

    update readme

--- resources/views/admin/components/sidebar.blade.php
+++ resources/views/admin/components/sidebar.blade.php
@@ -33,7 +33,7 @@
             </li>
 
             <li class="active">
-                <a href=""><i class="fa fa-th-large"></i> <span class="nav-label">System Logs Viewer</span>
+                <a href=""><i class="fa fa-database"></i> <span class="nav-label">System Logs Viewer</span>
                     <span class="fa arrow"></span></a>
                 <ul class="nav nav-second-level">
                     <li><a href="{{ route('admin.logs') }}">View Logs</a></li>
